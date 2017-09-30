3f525b528320a622e59521f61e15b10fcdf119f8
Bao Luu <baohtluu@gmail.com>
2017-09-30 10:38:11 -0500

    Adding manager ID into employee data when calling by employee id

--- app/Services/employee_function.php
+++ app/Services/employee_function.php
@@ -13,14 +13,14 @@ class employee_function{
 
   public function fetching_data_with_storeID($storeID){
     $dbConnection = new employee();
-    $data = $dbConnection::where("WorkForStoreID", $storeID)->get();
+    $data = $dbConnection::where("WorkForStoreID", $storeID)->with('manager')->get();
     return $data;
 
   }
 
   public function fetching_data_with_employeeID($employeeID){
     $dbConnection = new employee();
-    $data = $dbConnection::where("EID", $employeeID)->get();
+    $data = $dbConnection::where("EID", $employeeID)->with('manager')->get();
     return $data;
 
   }
